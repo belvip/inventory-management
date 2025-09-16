7b2d9cc4493e76e2dc8d123d40c2a69bacc3816c
belvinard <belvi757@gmail.com>
2025-09-16 10:45:57 +0100

    feat: Add security to category controller endpoint

--- src/main/java/com/belvinard/inventory_management/controller/CategoryController.java
+++ src/main/java/com/belvinard/inventory_management/controller/CategoryController.java
@@ -15,6 +15,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -25,9 +26,8 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
-    @PostMapping("/create")
     @Operation(
-        summary = "Create a new category",
+        summary = "Create a new category - ADMIN and MANAGER",
         description = "Creates a new category and associates it with the company specified in the request body.",
         responses = {
             @ApiResponse(responseCode = "201", description = "Category created successfully",
@@ -37,6 +37,8 @@ public class CategoryController {
             @ApiResponse(responseCode = "400", description = "Invalid request body")
         }
     )
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+    @PostMapping("/create")
     public ResponseEntity<CategoryResponseDto> createCategory(
             @io.swagger.v3.oas.annotations.parameters.RequestBody(
                 description = "Category information to be created (including companyId)",
@@ -50,11 +52,12 @@ public class CategoryController {
     }
 
     @Operation(
-            summary = "Get Category by ID",
+            summary = "Get Category by ID - Only ADMIN, MANAGER, SALES",
             description = "Fetch a single category using its unique identifier"
     )
     @ApiResponse(responseCode = "200", description = "Category found")
     @ApiResponse(responseCode = "404", description = "Category not found")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_SALES') or hasRole('ROLE_MANAGER')")
     @GetMapping("/{id}")
     public ResponseEntity<CategoryResponseDto> getCategoryById(
             @Parameter(description = "Category ID", example = "1")
@@ -67,12 +70,13 @@ public class CategoryController {
      * Update an existing category.
      */
     @Operation(
-            summary = "Update Category",
+            summary = "Update Category - ADMIN and MANAGER",
             description = "Update the details of an existing category"
     )
     @ApiResponse(responseCode = "200", description = "Category updated successfully")
     @ApiResponse(responseCode = "404", description = "Category not found")
     @ApiResponse(responseCode = "409", description = "Category with the same code already exists")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
     @PutMapping("/{id}")
     public ResponseEntity<CategoryResponseDto> updateCategory(
             @Parameter(description = "Category ID to update", example = "1")
@@ -86,11 +90,12 @@ public class CategoryController {
      * Delete a category by ID.
      */
     @Operation(
-            summary = "Delete Category",
+            summary = "Delete Category - ONLY ADMIN",
             description = "Delete an existing category by its ID"
     )
     @ApiResponse(responseCode = "204", description = "Category deleted successfully")
     @ApiResponse(responseCode = "404", description = "Category not found")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCategory(
             @Parameter(description = "Category ID to delete", example = "1")
@@ -104,11 +109,12 @@ public class CategoryController {
      * Get category by Company ID.
      */
     @Operation(
-            summary = "Get Category by Company ID",
+            summary = "Get Category by Company ID - ADMIN, MANAGER - SALES",
             description = "Fetch a category that belongs to a specific company"
     )
     @ApiResponse(responseCode = "200", description = "Category found for company")
     @ApiResponse(responseCode = "404", description = "Company or category not found")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_SALES') or hasRole('ROLE_MANAGER')")
     @GetMapping("/by-company/{companyId}")
     public ResponseEntity<CategoryResponseDto> getCategoryByCompanyId(
             @Parameter(description = "Company ID to search for categories", example = "5")
@@ -117,12 +123,13 @@ public class CategoryController {
         return ResponseEntity.ok(categoryService.getCategoryByCompanyId(companyId));
     }
 
-    @GetMapping("/all")
     @Operation(
             summary = "Get All Categories",
             description = "Fetch all categories with pagination and sorting (default sort by companyId)"
     )
     @ApiResponse(responseCode = "200", description = "Categories retrieved successfully")
+    @PreAuthorize("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+    @GetMapping("/all")
     public ResponseEntity<PagedResponse<CategoryResponseDto>> getAllCategories(
             @Parameter(description = "Page number (0-based)", example = "0")
             @RequestParam(value = "pageNumber", defaultValue = AppConstant.PAGE_NUMBER, required = false) Integer pageNumber,
