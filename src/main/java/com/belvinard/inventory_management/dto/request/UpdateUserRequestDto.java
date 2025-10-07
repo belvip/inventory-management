58d5f8be5997c94cd6d0822a24b403f7b10f1380
belvinard <belvi757@gmail.com>
2025-10-07 08:10:24 +0100

    update user

---
+++ src/main/java/com/belvinard/inventory_management/dto/request/UpdateUserRequestDto.java
@@ -0,0 +1,33 @@
+package com.belvinard.inventory_management.dto.request;
+
+
+import com.belvinard.inventory_management.dto.AddressDto;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record UpdateUserRequestDto(
+        @NotBlank(message = "First name is required")
+        @Size(min = 3, max = 20, message = "First name must be between 3 and 20 characters")
+        String firstName,
+
+        @NotBlank(message = "Last name is required")
+        @Size(min = 3, max = 20, message = "Last name must be between 3 and 20 characters")
+        String lastName,
+
+        @NotBlank(message = "Username is required")
+        @Size(min = 4, max = 10, message = "Username must be between 3 and 10 characters")
+        String userName,
+
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email must be valid")
+        @Size(max = 50, message = "Email must not exceed 50 characters")
+        String email,
+
+        @Valid
+        AddressDto address
+
+
+) {
+}
