4f526c27e806b007bef8cbe4f0fd0e068a0f0df5
belvip <pouadjeubelvi@gmail.com>
2025-11-08 11:00:34 +0000

    client phone update

--- src/main/java/com/belvinard/inventory_management/dto/request/ClientRequestDto.java
+++ src/main/java/com/belvinard/inventory_management/dto/request/ClientRequestDto.java
@@ -17,7 +17,7 @@ public record ClientRequestDto(
         String email,
 
         @Pattern(
-                regexp = "^(?:(?:\\+237|237)[-.\\s]?)?(?:(?:[67][25-9]\\d{7})|(?:2\\d{2}\\d{6}))$",
+                regexp = "^$|^(?:(?:\\+237|237)[-.\\s]?)?(?:[67][25-9]\\d{7}|2\\d{2}\\d{6})$",
                 message = "The phone number must be a valid Cameroonian number (mobile or fixed). Examples: 671234567, 222123456, +237-233123456"
         )
         String phoneNumber
