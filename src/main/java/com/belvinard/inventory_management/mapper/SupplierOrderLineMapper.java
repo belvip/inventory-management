460253c5fcbb683e05edc1708201353a18f4ad39
belvip <pouadjeubelvi@gmail.com>
2025-12-03 14:33:13 +0000

    ad supplier ID

--- src/main/java/com/belvinard/inventory_management/mapper/SupplierOrderLineMapper.java
+++ src/main/java/com/belvinard/inventory_management/mapper/SupplierOrderLineMapper.java
@@ -9,6 +9,7 @@ import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface SupplierOrderLineMapper {
+    @Mapping(source = "supplierOrder.supplier.id", target = "supplierId")
     @Mapping(source = "article.id", target = "articleId")
     @Mapping(source = "article.codeArticle", target = "articleCode")
     @Mapping(source = "article.designation", target = "articleDesignation")
